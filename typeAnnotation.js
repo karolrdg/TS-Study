905751b741460ce680ea293f565be63fe91bcac7
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-09-11 10:27:58 -0300

    type-annotation

---
+++ typeAnnotation.js
@@ -0,0 +1,11 @@
+//Array
+var animais = ['Elefante', 'Cachorro', 'Gato'];
+console.log(animais);
+//Objeto
+var carro = { nome: 'Fusca', ano: 1969 };
+console.log(carro);
+//Função
+function multiplicar(a, b) {
+    return a * b;
+}
+console.log(multiplicar(4, 7));
---
+++ typeAnnotation.ts
@@ -0,0 +1,13 @@
+//Array
+let animais: string[] = ['Elefante', 'Cachorro', 'Gato'];
+console.log(animais); 
+
+//Objeto
+let carro : {nome: string, ano: number} = {nome: 'Fusca', ano: 1969};
+console.log(carro);
+
+//Função
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+console.log(multiplicar(4, 7));
