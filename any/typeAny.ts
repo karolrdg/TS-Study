dd7dbd1b8f28cdfccea87c9d095b7b50b4c7684a
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-09-16 02:18:21 -0300

    feat(any): add examples demonstrating the use of 'any' type in TypeScript

---
+++ any/typeAny.ts
@@ -0,0 +1,21 @@
+const a: any = 34;
+const b: any = ['Glaucia'];
+
+const result = a + b;
+console.log(result);
+
+// ==> Exemplo 02: Quando o tipo 'any' é inferido implícitamente
+/*let frase;
+frase = 'Oi, pessoal! Tudo bem?';
+
+console.log(frase);*/
+
+// ==> Exemplo 03: Quando devemos usar o tipo any?!
+
+const formulario: { [campoFomulario: string]: any } = {
+  nome: 'Glaucia',
+  sobrenome: 'Lemos',
+  idade: 34,
+};
+
+console.log(formulario);
