3489679e7c0bfeaf4323246d8c3c08fdb75b1765
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-09-17 14:26:53 -0300

    feat(never): add examples demonstrating the use of the never type

--- never/typeNever.ts
+++ never/typeNever.ts
@@ -26,3 +26,56 @@ function checkType(value: SomeType): string {
 }
 
 */
+
+/*// ==> Exemplo 01: Never - Throw Exception
+
+function error(message: string): never {
+    throw new Error(message);
+  }
+  
+  console.log(error('Erro de Mensagem - 01'));
+  
+  // ==> Exemplo 02: Never inferido automaticamente
+  function rejectMessage() {
+    return error('Error de Mensagem - 02');
+  }
+  
+  console.log(rejectMessage());
+  
+  // ==> Exemplo 03 - Função que contém loop infinito retorna o tipo 'never'
+  
+  const loopInfinito = function loop() {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      console.log('Oi, Developers!');
+    }
+  };
+  
+  // console.log(loopInfinito());
+  
+  // ==> Exemplo 04: Diferença entre os tipos: 'never' x 'void'
+  
+  const algumaCoisaVoid: void = null;
+  
+  // ==> dará erro! (// Error: Type 'null' is not assignable to type 'never')
+  // const algumaCoisaNever: never = null;
+  
+  //console.log(algumaCoisaVoid);
+  //console.log(algumaCoisaNever);
+*/
+/* //ex
+  function caixaMagica(mensagem: string): never {
+    throw new Error(mensagem);
+  }
+  
+  console.log(caixaMagica('Ocorreu um erro!'));*/
+
+//ex
+const caixaInfinita = function () {
+  // Loop infinito, como uma caixa mágica que nunca para
+  while (true) {
+    console.log('Esta caixa nunca para!');
+  }
+};
+
+//console.log(caixaInfinita());
