08d500b6cebf27eb5eaa4c087ec24643b796f09a
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-01 09:54:49 -0300

    refactor(satisfies-operator): update satisfiesOperator.js and satisfiesOperator.ts
    Update satisfiesOperator.js and satisfiesOperator.ts

--- satisfies-operator/satisfiesOperator.js
+++ satisfies-operator/satisfiesOperator.js
@@ -7,3 +7,13 @@ var pessoa = {
 }; // Verificação de que o objeto atende às regras do tipo Pessoa
 // Imprimindo o nome da cidade de nascimento em maiúsculas
 console.log(pessoa.localNascimento.toUpperCase()); // IÇARA
+var config = {
+    host: "localhost",
+    port: 3306,
+    tryReconnect: function () { return true; },
+    poolSize: 10,
+};
+function connect() {
+    var host = config.host, port = config.port, tryReconnect = config.tryReconnect;
+    createConnection(host, "".concat(port), tryReconnect(), 10);
+}
--- satisfies-operator/satisfiesOperator.ts
+++ satisfies-operator/satisfiesOperator.ts
@@ -30,3 +30,33 @@ const pessoa = {
 
 // Imprimindo o nome da cidade de nascimento em maiúsculas
 console.log(pessoa.localNascimento.toUpperCase()); // IÇARA
+
+// outro ex
+type Connection = {};
+
+declare function createConnection(
+  host: string,
+  port: string,
+  reconnect: boolean,
+  poolsize: number
+): Connection;
+
+type Configuration = {
+  host: string;
+  port: string | number;
+  tryReconnect: boolean | (() => boolean);
+  poolSize?: number;
+};
+
+const config = {
+  host: "localhost",
+  port: 3306,
+  tryReconnect: () => true,
+  poolSize: 10,
+} satisfies Configuration;
+
+function connect() {
+  let { host, port, tryReconnect } = config;
+
+  createConnection(host, `${port}`, tryReconnect(), 10);
+}
