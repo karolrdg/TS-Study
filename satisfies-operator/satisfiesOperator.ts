8ff43000d9556abc4e7af816e39919d6cfc81884
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-01 09:29:06 -0300

    refactor(satisfies-operator): update e

--- satisfies-operator/satisfiesOperator.ts
+++ satisfies-operator/satisfiesOperator.ts
@@ -29,4 +29,4 @@ const pessoa = {
 } satisfies Pessoa; // Verificação de que o objeto atende às regras do tipo Pessoa
 
 // Imprimindo o nome da cidade de nascimento em maiúsculas
-console.log(pessoa.localNascimento.toUpperCase()); //"RIO DE JANEIRO"
+console.log(pessoa.localNascimento.toUpperCase()); // IÇARA
